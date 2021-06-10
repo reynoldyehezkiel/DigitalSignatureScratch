5a38b06f8c32558f8cdd697d743484379f652005
reynoldyehezkiel <73789263+reynoldyehezkiel@users.noreply.github.com>
2021-06-10 16:39:26 +0700

    Add Back Button Function

--- app/src/main/java/com/app/digitalsignature/PDFViewerActivity.kt
+++ app/src/main/java/com/app/digitalsignature/PDFViewerActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_pdfviewer.*
@@ -13,6 +14,8 @@ class PDFViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdfviewer)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (intent != null) {
             val viewType = intent.getStringExtra("ViewType")
@@ -51,6 +54,15 @@ class PDFViewerActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
